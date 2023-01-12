246ac037d372804cae6d2eceb6d202810858e342
Luckas Rakoto <luckasrakoto@gmail.com>
2023-01-12 10:14:55 -0500

    Bootstrap styling works

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { auth0Domain, auth0ClientId } from './config';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
